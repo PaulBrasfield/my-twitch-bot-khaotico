8c45849e06270bb684fb884d5445949b9d3960a7
Paul <paulbrasfield@hotmail.com>
2022-01-24 20:40:55 -0600

    Code cleanup

--- server.js
+++ server.js
@@ -1,48 +1,76 @@
-require('dotenv').config();
 
-const tmi = require('tmi.js');
+/*-------------------------- ANCIENT EGG BOT --------------------------
+    |  AUTHOR: Paul Brasfield
+    |
+    |  CREATED:  Jan 24, 2022
+    |
+    |  (c) Copyright by Khaotico
+/*-------------------------------------------------------------------*/
 
-const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+require('dotenv').config(); //Environment file to store sensitive data
 
-var currentTime; // = date.toLocaleTimeString('en-US', { timeZone: 'America/Chicago', hour: '2-digit', minute:'2-digit'}) + " CST"
+const tmi = require('tmi.js'); //Library to communicate with Twitch API
 
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/); //Regular Expression for command usage
+
+const VERSION = 'v1.0.0'; //Current version of the bot
+
+var currentTime; //The current system time
+
+//List of commands
 const commands = {
-    twitter: {
-        response: 'https://twitter.com/KhaoticoTTV/'
-    },
-    youtube: {
-        response: 'https://www.youtube.com/channel/UC803hb0MzuN1Mz1W6_iUu-w'
-    },
-    friendcode: {
-        response: 'Switch Friend Code: SW-3714-0961-8601'
-    },
-    refund: {
-        response: (user) => `${user} Your channel points PepeLaugh :point_right: :chart_with_downwards_trend:`
-    },
+
+    //General Commands
     schedule: {
         response: 'Mondays, Wednesdays, and Fridays from 8pm CST to 12am-1am CST'
-    },
-    believers: {
-        response: 'BELIEVERS :clown_face:'
-    },
-    doubters: {
-        response: 'DOUBTERS :clown_face:'
     },
     time: {
         response: `Current time for Khaotico: ${currentTime}`
     },
-    villagers: {
-        response: 'Current Villagers in Aria: Annabelle, Cranston, Doc, Dom, Ed, Eugene, Marina, Piper, Tad'
+
+    //Spam Commands
+    refund: {
+        response: (user) => `${user} Your channel points PepeLaugh :point_right: :chart_with_downwards_trend:`
     },
     xboxblowout: {
         response: 'By the rivers of Babylon, there we sat down, yea, we wept, when we remembered Zion. ' + 
         'Remember, O Lord, The Children of Edom in the day of Jerusalem who said, "Raze it, raze it, ' + 
         'even to the foundation." O daughter of Babylon, who art to be destroyed. ' + 
         'How happy shall he be, that rew https://twitter.com/pauljac3_/status/984445523326263296?s=20'
+    },
+
+    //Game Commands
+
+        //Animal Crossing New Horizons
+        villagers: {
+            response: 'Current Villagers in Aria: Annabelle, Cranston, Doc, Dom, Ed, Eugene, Marina, Piper, Tad'
+        },
+   
+
+    //Social Commands
+    friendcode: {
+        response: 'Switch Friend Code: SW-3714-0961-8601'
+    },
+    discord: {
+        response: 'Join Khaotico\'s Discord Server! https://discord.gg/WSaeZEcX7J'
+    },
+    twitter: {
+        response: 'https://twitter.com/KhaoticoTTV/'
+    },
+    youtube: {
+        response: 'https://www.youtube.com/channel/UC803hb0MzuN1Mz1W6_iUu-w'
+    },
+
+    //Debugging Commands
+    ping: {
+        response: (user) => `Hello ${user}, I am active!`
+    },
+    version: {
+        response: `I am in version ${VERSION}`
     }
 }
 
-
+//Create a new Client object from TMI
 const client = new tmi.Client({
     connection: {
       reconnect: true
@@ -56,46 +84,55 @@ const client = new tmi.Client({
     }
   });
 
+
+//Connect to the channel
 client.connect();
 
+//Print messages if we successfully connected
 client.on('connected', () => {
-    console.log("Ancient Egg Bot v1.0")
-    console.log("--------------------\n")
-    console.log("\nConnected to the channel successfully!\n")
+    console.log(`Ancient Egg Bot ${VERSION}`)
+    console.log("-----------------------")
+    console.log("\nConnected to the channel successfully!")
     
     getTime();
+
+    console.log("-----------------------")
 
     setInterval(getTime, 60000);
 })
 
-
+//Run commands based on messages received
 client.on('message', (channel, tags, message, self) => {
     
     const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
 
-      if ( !isNotBot ) return;
+    if ( !isNotBot ) {
+        return;
+    }
 
-      const [raw, command, argument] = message.match(regexpCommand);
+    const [raw, command, argument] = message.match(regexpCommand);
 
-      const { response } = commands[command] || {};
+    const { response } = commands[command] || {};
 
-      if ( typeof response === 'function' ) {
-        client.say(channel, response(tags.username))
-      } else if ( typeof response === 'string' ) {
-          client.say(channel, response);
+    if ( typeof response === 'function' ) {
+    client.say(channel, response(tags.username))
+    } else if ( typeof response === 'string' ) {
+    client.say(channel, response);
+    } else {
+        client.say(channel, 'yes')
       }
 
 	console.log(`${tags['display-name']}: ${message}`);
 });
 
+//Get current time and format according to 12-hour convention
 function getTime() {
     let date = new Date();
 
     currentTime = date.toLocaleTimeString('en-US', { timeZone: 'America/Chicago', hour: '2-digit', minute:'2-digit'}) + " CST"
     
-
     commands.time.response = `Current time for Khaotico: ${currentTime}`
-
-    console.log("The time is: " + currentTime)
+ 
+    console.log("\nThe time is: " + currentTime + "\n")
     
 }
